d4e580b924fc67d00f51c458834d66d621bfb1da
Yuku TAKAHASHI <taka84u9@gmail.com>
2015-02-07 21:58:03 +0900

    Implement BehaviorValidator.valid?

--- lib/behavior_validator.rb
+++ lib/behavior_validator.rb
@@ -1,2 +1,16 @@
 class BehaviorValidator < ActiveModel::EachValidator
+  class << self
+    def valid?(value, options = {})
+      options = options.reject { |option| reserved_options.include?(option) }
+      options.each_pair.all? do |method, expected_result|
+        value.__send__(method) == expected_result
+      end
+    rescue NoMethodError
+      false
+    end
+
+    def reserved_options
+      [:allow_nil, :allow_blank, :message, :on]
+    end
+  end
 end
